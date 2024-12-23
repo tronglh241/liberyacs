87c1329eb0e882d573498a673ef74ef57526f2ba
Trong Le Huu <tronglh.241@gmail.com>
2024-12-23 21:39:45 +0700

    Add tests

--- tests/cases/config.py
+++ tests/cases/config.py
@@ -41,3 +41,19 @@ class RegularCase(Case):
         assert config.date_object.day == 24
 
         assert config.dt_object == datetime.MINYEAR
+
+
+class NotEvalCase(Case):
+    config_file = 'tests/configs/not_eval.yml'
+    evaluate = False
+
+    def check(self):
+        config = self.load_config()
+
+        assert config.num_int == 10
+        assert config.num_float == 1.23
+        assert config.string == "'This is a string.'"
+
+        assert config.list[:4] == [1, 1.1, 'num_int', 'num_float']
+        assert config.list[4].sub_dict.num_int == 20
+        assert config.list[4].sub_dict.num_float == 2.34
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,12 +1,13 @@
 import pytest
 from cases.case import Case
-from cases.config import FileNotFoundCase, RegularCase
+from cases.config import FileNotFoundCase, NotEvalCase, RegularCase
 
 
 @pytest.mark.parametrize(
     'case', [
         FileNotFoundCase,
         RegularCase,
+        NotEvalCase,
     ]
 )
 def test_cfgnode_load(case: Case) -> None:
